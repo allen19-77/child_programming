fd6986b44040c7d12aa8e756cfa670a898f0e22c
allen19-77 <allen.19@list.ru>
2021-02-24 18:53:49 +0500

    mask for creditcard number

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-const firstNames = ['Gloria', 'Jenny', 'Emely', 'Kristina', 'Penelopa', 'Elen', 'Vanessa']
-const lastNames = ['Smith', 'Wolf', 'Adams', 'Perry', 'Agelera', 'Lopes', 'Reddys']
 
-function rnd(limit) {
-	return Math.floor(Math.random() * limit)
-}
+let myStr = '123456789121345456786'
 
-function fullName(firstName, lastName, count) {
+console.log(myStr)
 
-	let fullNames = []
-	for (let i = 0; i < count; i++) {
-		fullNames.push(firstName[rnd(firstName.length)] + ' ' + lastName[rnd(lastName.length)])
+let body = document.querySelector('body')
+let myTest = document.createElement('p')
+
+ function maskStr(str) {
+	let newStr = ''
+	for (let i = 0; i < str.length; i++ ) {
+		if ( i >= str.length - 4 ) newStr += str[i]
+		else newStr += '#'
+
 	}
-	return fullNames
+	return newStr
 }
+myTest.innerText = maskStr(myStr)
+body.appendChild(myTest)
 
-
-
-function list(item) {
-	let listItems = ''
-	for (let i = 0; i < item.length; i++) {
-		listItems += `<li>${item[i]}</li>`
-	}
-	document.body.innerHTML = `<ol>${listItems}</ol>`
-}
-
-list(fullName(firstNames, lastNames, 5))
